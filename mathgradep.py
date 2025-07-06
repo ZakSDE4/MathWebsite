66d06b13e493226056041344dce563a75eea8720
ZakWEBDV1 <zalsubai1@gmail.com>
2025-07-06 15:24:46 -0400

    Add input for grade

---
+++ mathgradep.py
@@ -0,0 +1,11 @@
+stu_mathg = 100
+if (stu_mathg >= 90):
+    print("You got a A on your Math Test, Amazing Job!")
+elif (stu_mathg >= 80):
+    print("You got a B on your Math Test, Nice Job!")
+elif (stu_mathg >= 70):
+    print("You got a C on your Math Test, Good Work!")
+elif (stu_mathg <= 60):
+    print("You got a F on your Math Test, Good Try, Study More and Youll Do Better.")
+else:
+    print("Invalid Score, Please Contact Your Teacher")
---
+++ studentgradelevel.py
@@ -0,0 +1,11 @@
+stu_age = 9
+if (stu_age == 14, 15, 16, 17, 18):
+    print("This student is in High School.")
+elif (stu_age == 9, 10, 11, 12, 13):
+    print("This student is in Middle School.")
+elif (stu_age < 9):
+    print("This student is in Primary School.")
+elif (stu_age == 0):
+    print("This fella is too young for School.")
+else:
+    print("Invalid Age")
